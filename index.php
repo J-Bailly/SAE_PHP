1121a72478d8d60b959d630d8213d9a020caf4e1
Julien <julien.bailly2@etu.univ-orleans.fr>
2025-03-04 21:23:44 +0100

    ajout des templates fonctionnel

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php require_once("app/views/template/template.php")
-
-
-    //foreach ($restaurants as $restaurant);
-    ?>
-    <div>
-        <h2><?= $restaurant->getName() ?></h2>
-        <p><?= $restaurant->getCuisineType() ?></p>
-    </div>
-<?php //endforeach; ?>
+<?php require_once("flavorya/views/template/template_accueil.php")?>
