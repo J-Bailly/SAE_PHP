cc11e8407be266bf528a86db0ecc8e9d3c4a1a82
Fbouam <143797329+Fbouam@users.noreply.github.com>
2025-03-08 22:33:39 +0100

    gestion du contrôleur d'inscription.

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php 
-require_once("app/views/template/template_accueil.php")?>
-<?php
+session_start();
 require_once "app/autoload.php";
 
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controllerName = ucfirst($_GET['controller']) . 'Controller';
+    $actionName = $_GET['action'];
+    $controllerClass = "app\\controllers\\" . $controllerName;
+    
+    if (class_exists($controllerClass)) {
+        $controller = new $controllerClass();
+        if (method_exists($controller, $actionName)) {
+            $controller->$actionName();
+            exit;
+        }
+    }
+}
+
 use app\controllers\RestaurantController;
+require_once("app/views/template/template_accueil.php");
 
 $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 $controller = new RestaurantController();
 
 // Gestion des routes
@@ -18,4 +31,4 @@ if ($requestPath == '/' || $requestPath == '/index.php') {
     http_response_code(404);
     echo "Page non trouvée";
 }
-
+?>
