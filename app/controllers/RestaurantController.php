f4982ab55065359d14425c1624e9f8e8c332f429
Amin El Mellouki <amin.el-mellouki@etu.univ-orleans.fr>
2025-03-08 12:53:29 +0100

    recherche fonctionnelle

--- app/controllers/RestaurantController.php
+++ app/controllers/RestaurantController.php
@@ -8,18 +8,24 @@ use app\services\jsonloader;
 class RestaurantController {
     
     public function index() {
-        // Chemin vers le fichier JSON
+        // Chemin vers les fichiers JSON
         $jsonRestaurant = __DIR__ . '/../../app/data/restaurants_orleans.json';
-        $jsonImage = __DIR__. '/../../app/data/restaurant_images.json';
-
+        $jsonImage = __DIR__ . '/../../app/data/restaurant_images.json';
+    
         // Charger les données JSON
         $restaurants = jsonloader::load($jsonRestaurant);
         $images = jsonloader::load($jsonImage);
-
-        // Limiter le nombre de restaurants à afficher
-        $restaurants = array_slice($restaurants, 0, 10);
-
-        // Récupérer l'adresse pour chaque restaurant
+    
+        $query = isset($_GET['search']) ? trim($_GET['search']) : '';
+    
+        if ($query !== '') {
+            $restaurants = array_filter($restaurants, function($restaurant) use ($query) {
+                return stripos($restaurant['name'], $query) !== false;
+            });
+        } else {
+            $restaurants = array_slice($restaurants, 0, 10);
+        }
+    
         foreach ($restaurants as &$restaurant) {
             foreach ($images as $image) {
                 if ($image['name'] == $restaurant['name']) {
@@ -28,10 +34,10 @@ class RestaurantController {
                 }
             }
         }
-
+    
         // Passer les données à la vue
         require_once __DIR__ . '/../views/restaurants/restaurant_list.php';
-    }
+    }    
 
     public function show($id) {
         // Chemin vers le fichier JSON
--- index.php
+++ index.php
@@ -1,23 +1,21 @@
-<?php require_once("app/views/template/template_accueil.php")?>
+<?php 
+require_once("app/views/template/template_accueil.php")?>
 <?php
-
 require_once "app/autoload.php";
 
 use app\controllers\RestaurantController;
 
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
 $controller = new RestaurantController();
 
 // Gestion des routes
-$request = $_SERVER['REQUEST_URI'];
-switch ($request) {
-    case '/':
-        $controller->index();
-        break;
-    case (preg_match('/\/restaurant\/(\d+)/', $request, $matches) ? true : false):
-        $controller->show($matches[1]);
-        break;
-    default:
-        http_response_code(404);
-        echo "Page non trouvée";
-        break;
+if ($requestPath == '/' || $requestPath == '/index.php') {
+    $controller->index();
+} elseif (preg_match('/\/restaurant\/(\d+)/', $requestPath, $matches)) {
+    $controller->show($matches[1]);
+} else {
+    http_response_code(404);
+    echo "Page non trouvée";
 }
+
