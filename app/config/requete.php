2f78e30179b34619090cc1cb3f396bd5ffc1e4d4
LeoSaunier <leo.saunier@etu.univ-orleans.fr>
2025-03-08 12:26:40 +0100

    Requete de recherche de restaurant

--- app/config/requete.php
+++ app/config/requete.php
@@ -109,6 +109,28 @@ class Requete {
         return password_verify($password, $result['password_hash']);
     }
 
+    static public function seacrh_restaurant($search) {
+        $pdo = Database::getConnection();
+        $liste_restaurants = [];
+        
+        $sql = "SELECT * FROM public.".'"Restaurants"'." WHERE name ILIKE :search";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':search', '%'.$search.'%', PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $stmtCuisine = $pdo->prepare("SELECT name FROM public.".'"Restaurants_Cuisines"'." natural join public.".'"Cuisines"'." WHERE restaurant_id = :restaurant_id");
+        foreach ($result as $index => $restaurant) {
+            $stmtCuisine->bindValue(':restaurant_id', $restaurant['restaurant_id'], PDO::PARAM_INT);
+            $stmtCuisine->execute();
+            $cuisines = $stmtCuisine->fetchAll(PDO::FETCH_ASSOC);
+            $new_restaurant = new Restaurant($restaurant, $cuisines);
+            $liste_restaurants[$index] = $new_restaurant;
+        }
+
+        return $liste_restaurants;
+    }
+
 }
 
 
