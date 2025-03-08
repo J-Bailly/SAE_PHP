7c7d1132d8cf7e704beb887c9478e8464c9c0a62
LeoSaunier <leo.saunier@etu.univ-orleans.fr>
2025-03-08 11:11:07 +0100

    Recherche des restaurant en fonction de la cuisine + récupération de toute les cuisine présente

--- app/config/requete.php
+++ app/config/requete.php
@@ -49,8 +49,39 @@ class Requete {
         return $new_restaurant;
     }
 
-    
-    
+
+    static public function get_restaurants_by_cuisine($cuisine) {
+        $pdo = Database::getConnection();
+        $liste_restaurants = [];
+        
+        $sql = "SELECT * FROM public.".'"Restaurants"'." WHERE restaurant_id IN (SELECT restaurant_id FROM public.".'"Restaurants_Cuisines"'." natural join public.".'"Cuisines"'." WHERE name = :cuisine)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':cuisine', $cuisine, PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $stmtCuisine = $pdo->prepare("SELECT name FROM public.".'"Restaurants_Cuisines"'." natural join public.".'"Cuisines"'." WHERE restaurant_id = :restaurant_id");
+        foreach ($result as $index => $restaurant) {
+            $stmtCuisine->bindValue(':restaurant_id', $restaurant['restaurant_id'], PDO::PARAM_INT);
+            $stmtCuisine->execute();
+            $cuisines = $stmtCuisine->fetchAll(PDO::FETCH_ASSOC);
+            $new_restaurant = new Restaurant($restaurant, $cuisines);
+            $liste_restaurants[$index] = $new_restaurant;
+        }
+
+        return $liste_restaurants;
+        
+    }
+
+    static public function get_cuisines() {
+        $pdo = Database::getConnection();
+        $sql = "SELECT name FROM public.".'"Cuisines"';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+        
+        return $result;
+    }
 
 }
 
